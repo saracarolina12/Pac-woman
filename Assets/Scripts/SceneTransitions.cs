25ae72036dbb1584cee92f5c6de261060178a5f1
niriot-dotcom <lopezpatyyarely@gmail.com>
2022-06-17 02:14:37 -0500

    fade transition for menus.

---
+++ Assets/Scripts/SceneTransitions.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneTransitions : MonoBehaviour
+{
+    public Animator transitionAnim;
+    public string sceneName;
+
+    void Start() {
+        
+    }
+}
