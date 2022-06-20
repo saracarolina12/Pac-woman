65774f2c9c44f3b400357b6b70515dd1fee92c7a
sara_carolina12 <saracarogode@gmail.com>
2022-06-19 22:46:42 -0500

    '90%'

--- Assets/Scripts/AI_Zombie.cs
+++ Assets/Scripts/AI_Zombie.cs
@@ -32,7 +32,7 @@ public class AI_Zombie : MonoBehaviour
     }
 
     public void Comportamiento_Enemigo(){
-        if(Vector3.Distance(transform.position, target.transform.position) > 3) //if the player is out of sight of the enemy
+        if(Vector3.Distance(transform.position, target.transform.position) > 5) //if the player is out of sight of the enemy
         {
             ani.SetBool("run", false);
             cronometro += 1*Time.deltaTime;
@@ -62,20 +62,36 @@ public class AI_Zombie : MonoBehaviour
                 lookPos.y = 0;
                 var rotation = Quaternion.LookRotation(lookPos);
                 transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, 2);
-                ani.SetBool("walk", false);
-                ani.SetBool("run", true);
-                transform.Translate(Vector3.forward*2*Time.deltaTime);
+                
+                if (IsPlayerVisible()) {
+                    ani.SetBool("walk", false);
+                    ani.SetBool("run", true);
+                    transform.Translate(Vector3.forward*2*Time.deltaTime);
+                }
 
                 ani.SetBool("attack", false);
             }else{ //attack
-                Debug.Log("attack");
-                ani.SetBool("walk", false);
-                ani.SetBool("run", false);
+                if (IsPlayerVisible()) {
+                    Debug.Log("attack");
+                    ani.SetBool("walk", false);
+                    ani.SetBool("run", false);
 
-                ani.SetBool("attack", true);
-                atacando = true;
-                //Final_Ani();
+                    ani.SetBool("attack", true);
+                    atacando = true;
+                    //Final_Ani();
+                }
             }
         }
+    }
+
+    bool IsPlayerVisible() {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, Mathf.Infinity))
+        {
+            // Debug.DrawRay(transform.position, transform.forward * hit.distance, Color.yellow);
+            // Debug.Log("Did Hit" + hit.transform.gameObject.tag);
+            return (hit.transform.gameObject.tag == "Player");
+        }
+        return false;
     }
 }
